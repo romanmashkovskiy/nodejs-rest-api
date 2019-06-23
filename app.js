b8e99ea3d66e7013cbd2bbbbf938599280a50590
php vsegda <phpvsegda@gmail.com>
2019-06-23 17:44:06 +0300

    change to mongoose use

--- app.js
+++ app.js
@@ -1,45 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('./utils/cors');
-const MongoClient = require("mongodb").MongoClient;
-const objectId = require("mongodb").ObjectID;
+const mongoose = require("mongoose");
 
-const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
-
-let dbClient;
+mongoose.set('useFindAndModify', false);
 
 const app = express();
+const Schema = mongoose.Schema;
+
+const userScheme = new Schema({name: String, age: Number}, {versionKey: false});
+const User = mongoose.model("User", userScheme);
+
 const jsonParser = bodyParser.json();
 
 app.use(express.static(__dirname + '/public'));
 app.use(cors);
 
-mongoClient.connect((err, client) => {
-    if(err) return console.log(err);
-    dbClient = client;
-    app.locals.collection = client.db("usersdb").collection("users");
-    app.listen(4000, function(){
-        console.log("Сервер ожидает подключения...");
+// подключение
+mongoose.connect("mongodb://localhost:27017/usersdb", {useNewUrlParser: true}, (err) => {
+    if (err) return console.log(err);
+    app.listen(4000, function () {
+        console.log("Сервер ожидает подключения на 4000 порту ...");
     });
 });
 
 // получение списка данных
 app.get('/api/users', (req, res) => {
-    const collection = req.app.locals.collection;
-    collection.find({}).toArray(function(err, users){
-
-        if(err) return console.log(err);
+    User.find({}, (err, users) => {
+        if (err) return console.log(err);
         res.send(users)
     });
 });
 
 // получение одного пользователя по id
 app.get('/api/users/:id', (req, res) => {
-    const id = new objectId(req.params.id);
-    const collection = req.app.locals.collection;
-    collection.findOne({_id: id}, function(err, user){
-
-        if(err) return console.log(err);
+    const id = req.params.id;
+    User.findOne({_id: id}, (err, user) => {
+        if (err) return console.log(err);
         res.send(user);
     });
 });
@@ -50,24 +47,20 @@ app.post('/api/users', jsonParser, (req, res) => {
 
     const userName = req.body.name;
     const userAge = req.body.age;
-    const user = {name: userName, age: userAge};
+    const user = new User({name: userName, age: userAge});
 
-    const collection = req.app.locals.collection;
-    collection.insertOne(user, function(err, result){
-
-        if(err) return console.log(err);
+    user.save((err) => {
+        if (err) return console.log(err);
         res.send(user);
     });
 });
 
 // удаление пользователя по id
 app.delete('/api/users/:id', (req, res) => {
-    const id = new objectId(req.params.id);
-    const collection = req.app.locals.collection;
-    collection.findOneAndDelete({_id: id}, function(err, result){
+    const id = req.params.id;
+    User.findByIdAndDelete(id, (err, user) => {
 
-        if(err) return console.log(err);
-        let user = result.value;
+        if (err) return console.log(err);
         res.send(user);
     });
 });
@@ -76,16 +69,13 @@ app.delete('/api/users/:id', (req, res) => {
 app.put('/api/users', jsonParser, (req, res) => {
     if (!req.body.name || !req.body.age) return res.sendStatus(400);
 
-    const id = new objectId(req.body.id);
+    const id = req.body.id;
     const userName = req.body.name;
     const userAge = req.body.age;
+    const newUser = {age: userAge, name: userName};
 
-    const collection = req.app.locals.collection;
-    collection.findOneAndUpdate({_id: id}, { $set: {age: userAge, name: userName}},
-        {returnOriginal: false },function(err, result){
-
-            if(err) return console.log(err);
-            const user = result.value;
-            res.send(user);
-        });
+    User.findByIdAndUpdate(id, newUser, {new: true}, (err, user) => {
+        if (err) return console.log(err);
+        res.send(user);
+    });
 });
